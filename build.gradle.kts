0f1b46064b8e9ed41d67be7a5afce581f9ebd1ad
NONPLAYT <kidzania177@gmail.com>
2022-11-20 16:56:04 +0300

    Rollback (again)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
-import java.nio.charset.StandardCharsets
-
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
@@ -9,33 +6,26 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
-    }
+    maven("https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.605.7")
+    remapper("net.fabricmc:tiny-remapper:0.7.0:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.22")
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
 subprojects {
     apply(plugin = "java")
 
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
-        }
-    }
+    java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
 
     tasks.withType<JavaCompile>().configureEach {
-        options.encoding = StandardCharsets.UTF_8.name()
+        options.encoding = "UTF-8"
         options.release.set(17)
     }
 
     repositories {
-        mavenLocal()
         mavenCentral()
         maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://papermc.io/repo/repository/maven-public/")
@@ -43,6 +33,7 @@ subprojects {
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
         maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
         maven("https://jitpack.io")
     }
 }
@@ -53,13 +44,13 @@ paperweight {
     remapRepo.set("https://maven.fabricmc.net/")
     decompileRepo.set("https://files.minecraftforge.net/maven/")
 
-    useStandardUpstream("purpur") {
-        url.set(github("PurpurMC", "Purpur"))
-        ref.set(providers.gradleProperty("purpurRef"))
+    useStandardUpstream("pufferfish") {
+        url.set(github("pufferfish-gg", "Pufferfish"))
+        ref.set(providers.gradleProperty("pufferfishRef"))
 
         withStandardPatcher {
-            apiSourceDirPath.set("Purpur-API")
-            serverSourceDirPath.set("Purpur-Server")
+            apiSourceDirPath.set("pufferfish-api")
+            serverSourceDirPath.set("pufferfish-server")
 
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
