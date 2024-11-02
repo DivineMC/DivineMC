deacaf316eac806caf825ec1dd3e98cdc03439e7
NONPLAYT <76615486+NONPLAYT@users.noreply.github.com>
2024-11-02 16:43:01 +0300

    Update to 1.21.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
 plugins {
     java
     `maven-publish`
-    id("io.papermc.paperweight.patcher") version "1.7.3"
+    id("io.papermc.paperweight.patcher") version "1.7.4"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
