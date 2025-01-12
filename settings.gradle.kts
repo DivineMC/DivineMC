df2b2cc1dba7fd035a7a797dc2e4b1e8e5be9803
NONPLAYT <76615486+NONPLAYT@users.noreply.github.com>
2025-01-12 20:47:03 +0300

    update upstream and add commands

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,9 +12,29 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
+if (!file(".git").exists()) {
+    val errorText = """
+        
+        =====================[ ERROR ]=====================
+         The DivineMC project directory is not a properly cloned Git repository.
+         
+         In order to build DivineMC from source you must clone
+         the DivineMC repository using Git, not download a code
+         zip from GitHub.
+         
+         Built Gale jars are available for download at
+         https://github.com/DivineMC/DivineMC/actions
+         
+         See https://github.com/PaperMC/Paper/blob/main/CONTRIBUTING.md
+         for further information on building and modifying Paper forks.
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
 rootProject.name = "DivineMC"
 
-for (name in listOf("divinemc-api", "divinemc-server", "divinemc-api-generator")) {
+for (name in listOf("divinemc-api", "divinemc-server")) {
     val projName = name.lowercase(Locale.ENGLISH)
     include(projName)
     findProject(":$projName")!!.projectDir = file(name)
