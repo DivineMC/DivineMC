7288c14c511f22b2dad1f759b6daf3c9fe0072b8
NONPLAYT <76615486+NONPLAYT@users.noreply.github.com>
2025-01-18 00:48:20 +0300

    Updated Upstream (Purpur)
    Upstream has released updates that appear to apply and compile correctly
    
    Purpur Changes:

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java
-    id("io.papermc.paperweight.patcher") version "2.0.0-beta.13"
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.14"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
@@ -27,11 +27,6 @@ paperweight {
             upstreamPath = "paper-api"
             patchesDir = file("divinemc-api/paper-patches")
             outputDir = file("paper-api")
-        }
-        patchRepo("paperApiGenerator") {
-            upstreamPath = "paper-api-generator"
-            patchesDir = file("divinemc-api-generator/paper-patches")
-            outputDir = file("paper-api-generator")
         }
         patchDir("purpurApi") {
             upstreamPath = "purpur-api"
